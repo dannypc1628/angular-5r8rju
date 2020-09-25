d9c674f239a6d13e994fcaadf451e720fc30ac91
dannypc1628 <h152074716@gmail.com>
2020-09-25 13:30:04 +0800

    使用購物車服務

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router'
 
 import {products} from '../products'
+import { CartService } from '../cart.service';
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
@@ -9,12 +10,16 @@ import {products} from '../products'
 })
 export class ProductDetailsComponent implements OnInit {
   product;
-  constructor(private route:ActivatedRoute) { }
+  constructor(private route:ActivatedRoute, private cartService: CartService) { }
 
   ngOnInit() {
     this.route.paramMap.subscribe(parmas =>{
       this.product = products[+parmas.get('productId')];
     })
+  }
+   addToCart(product) {
+    this.cartService.addToCart(product);
+    window.alert('你的商品已經加進購物車!');
   }
 
 }
