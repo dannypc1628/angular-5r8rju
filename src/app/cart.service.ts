6a1e7802c10c3e133528139b149dd7ab3267872b
dannypc1628 <h152074716@gmail.com>
2020-09-26 14:30:49 +0800

    在cart.service.ts中使用httpClient拿到/assets/shipping.json

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from'@angular/common/http';
 
 //StackBlitz預設會在在 app.module.ts 中提供購物車服務。
 //你也可以像文件中改以@Injectable({ providedIn: 'root'})的打包優化的相依性注入方式。
 @Injectable()
 export class CartService {
   items = [];
-  constructor() { }
+  constructor(private http:HttpClient) { }
   
   addToCart(product) {
     this.items.push(product);
@@ -18,6 +19,10 @@ export class CartService {
   clearCart() {
     this.items = [];
     return this.items;
+  }
+
+  getShippingPrices(){
+    this.http.get('/assets/shipping.json');
   }
 
 }
